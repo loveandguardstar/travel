0b037ac1dd675df925b6c55761cccd22dc56fc42
loveandguardstar <1029856980@qq.com>
2018-10-19 13:47:30 +0800

    second-submit

---
+++ src/pages/home/home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <div class="hello">hello</div>
+      <router-link to="/list" class="hello">列表</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style scoped>
+  .hello {
+    font-size: 20px;
+  }
+</style>
