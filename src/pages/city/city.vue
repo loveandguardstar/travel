55bdf7602e90485025b699ff8969143b8bcb6fe7
loveandguardstar <1029856980@qq.com>
2018-10-26 09:30:26 +0800

    'city-router'

---
+++ src/pages/city/city.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <city-home></city-home>
+  </div>
+</template>
+
+<script>
+import cityHome from './component/header'
+
+export default {
+  name: 'city',
+  components: {
+    cityHome
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
