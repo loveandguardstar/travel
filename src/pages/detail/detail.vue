f2a540006f9efcb9389a42a0f94a4b1077248a0e
loveandguardstar <1029856980@qq.com>
2018-10-31 18:58:46 +0800

    detail-finish

--- src/pages/detail/detail.vue
+++ src/pages/detail/detail.vue
@@ -2,19 +2,44 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import detailBanner from './components/banner'
 import detailHeader from './components/header'
+import detailList from './components/list'
 
 export default {
   name: 'detail',
   components: {
     detailBanner,
-    detailHeader
+    detailHeader,
+    detailList
+  },
+  data () {
+    return {
+      list: [{
+        title: '成人票',
+        children: [{
+          title: '成人三馆联票',
+          children: [{
+            title: '成人三馆联票'
+          }]
+        }, {
+          title: '成人五馆联票'
+        }]
+      }, {
+        title: '学生票'
+      }, {
+        title: '儿童票'
+      }, {
+        title: '老人票'
+      }]
+    }
   }
 }
 </script>
@@ -22,5 +47,5 @@ export default {
 <style lang="stylus" scoped>
   .content
     height: 50rem
-    background: #25a4bb
+    background: #fff
 </style>
